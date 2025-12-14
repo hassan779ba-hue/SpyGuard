9f7a5bfd7735c462d8a10d329f0c3883b24f9027
Ali ahmad <hassan779ba@gmail.com>
2025-12-14 18:44:40 +0300

    Revert: remove duplicate worklets plugin (already in reanimated)

--- babel.config.js
+++ babel.config.js
@@ -14,7 +14,6 @@ module.exports = function (api) {
           extensions: [".ios.js", ".android.js", ".js", ".ts", ".tsx", ".json"],
         },
       ],
-      "react-native-worklets/plugin",
       "react-native-reanimated/plugin",
     ],
   };
